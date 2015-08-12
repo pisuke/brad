5a2db0135711c7964b7d526ff99525b970c84741
Francesco Anselmo <francesco.anselmo@arup.com>
2015-08-12 07:52:19 +0100

    Added grid export and tweaked camera export

--- io_radiance_grid_export.py
+++ io_radiance_grid_export.py
@@ -19,12 +19,12 @@
 # <pep8 compliant>
 
 bl_info = {
-    "name": "Export Radiance View Files",
-    "description": "Export Radiance View Files",
+    "name": "Export Radiance Calculation Grids",
+    "description": "Export Radiance Calculation Grids",
     "author": "Francesco Anselmo",
     "version": (0, 1),
     "blender": (2, 57, 0),
-    "location": "File > Export > Export Radiance View Files (.vf)",
+    "location": "File > Export > Export Radiance Calculation Grids (.pnt)",
     "warning": "",
     "wiki_url":"",
     "category": "Import-Export",
@@ -33,9 +33,53 @@ bl_info = {
 
 
 import bpy
+import os
+import bmesh
 from mathutils import Vector
 from os.path import splitext
 from math import degrees
+
+def writeVtkPolydata(filename, verts, faces):
+    #print verts[1]
+    #print faces[1]
+    vtkfile = open(filename, "w")
+        
+    #VTK legacy file header
+    vtkfile.write("# vtk DataFile Version 2.0\n")
+    vtkfile.write(filename+"\n")
+    vtkfile.write("ASCII\n")
+    vtkfile.write("DATASET POLYDATA\n")
+    vtkfile.write("POINTS "+ str(len(verts)) + " float\n")
+
+    #export points
+    print("exporting points")
+    for vert in verts:
+        #print "exporting " + str(vert)
+        line = ""
+        for coord in vert.co:
+            line +=  str(coord) + " "
+        #vtkfile.write(str(vert.co[0]) + " " + str(vert.co[1]) + " "  + str(vert.co[2]) + "\n")
+        vtkfile.write(line + "\n")
+    #status message
+    print("points exported")
+                    
+    #export polygons
+    print("exporting polygons")
+    total = 0
+    fips = [(face, index) for index, face in enumerate(faces)] # face index polygons
+    for fip in fips:
+        #if len(face.v) > 2:
+        total += (len(fip[0].vertices)+1)
+    vtkfile.write("POLYGONS "+ str(len(fips)) + " " + str(total) + "\n")
+    for fip in fips:
+        #print "exporting " + str(face)
+        num = len(fip[0].vertices)
+        line = str(num) + " "
+        #if len(face.v) > 2:
+        for vertex in fip[0].vertices:
+            line +=  str(vertex) + " "
+        vtkfile.write(line + "\n")
+    vtkfile.close()
 
 def write_cameras(context, filepath, frame_start, frame_end, only_selected=False):
 
@@ -108,14 +152,14 @@ from bpy_extras.io_utils import ExportHelper
 
 
 class CameraExporter(bpy.types.Operator, ExportHelper):
-    """Export Blender Cameras to Radiance Views"""
+    """Export Radiance Calculation Grids"""
     
-    bl_idname = "export.radiance_views"
+    bl_idname = "export.radiance_grids"
 
-    bl_label = "Export Radiance View Files"
+    bl_label = "Export Radiance Calculation Grids"
 
-    filename_ext = ".vf"
-    filter_glob = StringProperty(default="*.vf", options={'HIDDEN'})
+    filename_ext = ".pnt"
+    filter_glob = StringProperty(default="*.pnt", options={'HIDDEN'})
 
     frame_start = IntProperty(name="Start Frame",
             description="Start frame for export",
@@ -143,7 +187,7 @@ class CameraExporter(bpy.types.Operator, ExportHelper):
 def menu_export(self, context):
     import os
     default_path = os.path.splitext(bpy.data.filepath)[0] + ".py"
-    self.layout.operator(CameraExporter.bl_idname, text="Radiance View Files (.vf)").filepath = default_path
+    self.layout.operator(CameraExporter.bl_idname, text="Radiance Calculation Grids (.pnt)").filepath = default_path
 
 
 def register():
